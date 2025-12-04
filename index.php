771a8973f9a29d8d5517c12a557bb35f9d72f594
leticiamagre <leticia.marcusso@aluno.senai.br>
2025-12-04 09:40:28 -0300

    autenticar login

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ if (!isset($_SESSION['usuario_id'])) {
     header('Location:login.php');
    exit;
 } 
+
+$usuario_id = $_SESSION['usuario_id'];
+$usuario_nome = $_SESSION['usuario_nome'];
 ?>
 
 <!DOCTYPE html>
@@ -17,6 +20,10 @@ if (!isset($_SESSION['usuario_id'])) {
 </head>
 <body>
     <h1>Sistema Financeiro</h1>
+
+    <div>
+        <p>Bem-Vindo, <strong> <?php echo $usuario_nome?></strong></p>
+    </div>
 
     
 </body>
